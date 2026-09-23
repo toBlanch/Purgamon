dbcbac6aa450ddf4415973505618f880aa1cae9d
Blanch <priorsfarley@protonmail.com>
2026-09-23 18:06:20 +0100

    Finished implementation for Skeletal Stance and Bone Fragments

---
+++ data/cobblemon/moves/bonefragments.js
@@ -0,0 +1,34 @@
+({
+    accuracy: true,
+    basePower: 0,
+    category: "Status",
+    name: "Bone Fragments",
+    pp: 20,
+    priority: 0,
+    flags: { reflectable: 1, metronome: 1, mustpressure: 1 },
+    sideCondition: "bonefragments",
+    condition: {
+      onSideStart(side) {
+        this.add("-sidestart", side, "move: Bone Fragments");
+        this.effectState.time = 3;
+      },
+      onEntryHazard(pokemon) {
+        if (!pokemon.isGrounded())
+          return;
+        if (!pokemon.hasItem("heavydutyboots")) {
+          const typeMod = this.clampIntRange(pokemon.runEffectiveness(this.dex.getActiveMove("bonefragments")), -6, 6);
+          this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);
+        }
+        this.effectState.time -= 1;
+        if (this.effectState.time == 0) {
+          this.add("-sideend", pokemon.side, "move: Bone Fragments", "[of] " + pokemon);
+          pokemon.side.removeSideCondition("bonefragments");
+        }
+      }
+    },
+    secondary: null,
+    target: "foeSide",
+    type: "Ground",
+    zMove: { boost: { def: 1 } },
+    contestType: "Cool"
+})
--- data/cobblemon/moves/skeletalstance.js
+++ data/cobblemon/moves/skeletalstance.js
@@ -1,13 +1,61 @@
 ({
-    accuracy: 1,
-    basePower: 1,
-    category: "Physical",
+    accuracy: true,
+    basePower: 0,
+    category: "Status",
     name: "Skeletal Stance",
-    pp: 64,
-    priority: 0,
-    flags: { contact: 1, protect: 1, mirror: 1, distance: 1, metronome: 1 },
+    pp: 10,
+    priority: 4,
+    flags: { noassist: 1, failcopycat: 1 },
+    stallingMove: true,
+    volatileStatus: "skeletalstance",
+    onPrepareHit(pokemon) {
+      return !!this.queue.willAct() && this.runEvent("StallMove", pokemon);
+    },
+    onHit(pokemon) {
+      pokemon.addVolatile("stall");
+    },
+    condition: {
+      duration: 1,
+      onStart(target) {
+        this.add("-singleturn", target, "move: Protect");
+      },
+      onTryHitPriority: 3,
+      onTryHit(target, source, move) {
+        if (!move.flags["protect"]) {
+          if (["gmaxoneblow", "gmaxrapidflow"].includes(move.id))
+            return;
+          if (move.isZ || move.isMax)
+            target.getMoveHitData(move).zBrokeProtect = true;
+          return;
+        }
+        if (move.smartTarget) {
+          move.smartTarget = false;
+        } else {
+          this.add("-activate", target, "move: Protect");
+        }
+        const lockedmove = source.getVolatile("lockedmove");
+        if (lockedmove) {
+          if (source.volatiles["lockedmove"].duration === 2) {
+            delete source.volatiles["lockedmove"];
+          }
+        }
+        if (this.checkMoveMakesContact(move, source, target)) {
+          for (const side of target.side.foeSidesWithConditions()) {
+            side.addSideCondition("bonefragments");
+          }
+          this.boost({ def: -1, }, target);
+        }
+        return this.NOT_FAIL;
+      },
+      onHit(target, source, move) {
+        if (move.isZOrMaxPowered && this.checkMoveMakesContact(move, source, target)) {
+          source.trySetStatus("psn", target);
+        }
+      }
+    },
     secondary: null,
-    target: "any",
-    type: "Ground",
-    contestType: "Cool"
+    target: "self",
+    type: "Poison",
+    zMove: { boost: { def: 1 } },
+    contestType: "Tough"
 })
